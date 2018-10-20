3a9c9e3b07e600d9e458f41a1364dc2420098e89
jamiebdavis <jamied1985@gmail.com>
2018-10-20 18:24:38 +0100

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://jamiebdavis:linkin1@ds237373.mlab.com:37373/dev-connector"
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const monogoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+//DB config
+const db = require("./config/keys").mongoURI;
+
+monogoose
+  .connect(
+    "mongodb://jamiebdavis:linkin1@ds237373.mlab.com:37373/dev-connector",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("mongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("hello world yt");
+});
+
+//use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log(`Sever running on port ${port}`);
+});
